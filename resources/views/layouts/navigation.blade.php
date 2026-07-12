de54fc725160c7c43cac57f5e475b65a9a7345a1
thomas <thomasaquino746@gmail.com>
2026-07-13 05:42:49 +0700

    Add navigation menu for StockEase

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,10 +12,32 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
-                </div>
+
+    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+        Dashboard
+    </x-nav-link>
+
+    <x-nav-link :href="route('categories.index')" :active="request()->routeIs('categories.*')">
+        Kategori
+    </x-nav-link>
+
+    <x-nav-link :href="route('products.index')" :active="request()->routeIs('products.*')">
+        Produk
+    </x-nav-link>
+
+    <x-nav-link :href="route('incoming-products.index')" :active="request()->routeIs('incoming-products.*')">
+        Barang Masuk
+    </x-nav-link>
+
+    <x-nav-link :href="route('outgoing-products.index')" :active="request()->routeIs('outgoing-products.*')">
+        Barang Keluar
+    </x-nav-link>
+
+    <x-nav-link :href="route('reports.incoming')" :active="request()->routeIs('reports.incoming')">
+        Laporan
+    </x-nav-link>
+
+</div>
             </div>
 
             <!-- Settings Dropdown -->
@@ -67,10 +89,32 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
-            </x-responsive-nav-link>
-        </div>
+
+    <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+        Dashboard
+    </x-responsive-nav-link>
+
+    <x-responsive-nav-link :href="route('categories.index')" :active="request()->routeIs('categories.*')">
+        Kategori
+    </x-responsive-nav-link>
+
+    <x-responsive-nav-link :href="route('products.index')" :active="request()->routeIs('products.*')">
+        Produk
+    </x-responsive-nav-link>
+
+    <x-responsive-nav-link :href="route('incoming-products.index')" :active="request()->routeIs('incoming-products.*')">
+        Barang Masuk
+    </x-responsive-nav-link>
+
+    <x-responsive-nav-link :href="route('outgoing-products.index')" :active="request()->routeIs('outgoing-products.*')">
+        Barang Keluar
+    </x-responsive-nav-link>
+
+    <x-responsive-nav-link :href="route('reports.incoming')" :active="request()->routeIs('reports.incoming')">
+        Laporan
+    </x-responsive-nav-link>
+
+</div>
 
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200">
