b96753e956a09b30fb03c08b150eb8b45513b468
thomas <thomasaquino746@gmail.com>
2026-07-13 00:14:26 +0700

    Redirect homepage to login

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('dashboard');
 });
 
 Route::get('/dashboard', [DashboardController::class, 'index'])
